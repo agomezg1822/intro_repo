ca51924bce086e8a9f2fc0dd6054484bcfe4cebd
Andres Gomez <andres_felipe@hotmail.com>
2023-03-03 06:42:33 -0500

    20230303 v1.0.2

--- repo.js
+++ repo.js
@@ -1,7 +1,7 @@
 //Estado del respositorii
 //U: No se tiene seguimiento (untracked)
-
 //tiene 3 areas de trabajo, work, staginin, repo
-//este es otro ambio del repositorio
+
+//este es otro cambio del repositorio
 //puede tener otro estado 
 //M:modificado
